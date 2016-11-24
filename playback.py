5a28a7a0a6078b58a488575eadc14a9614b61bf4
Colin Burke <c3g6b2@gmail.com>
2016-11-24 12:00:46 -0500

    Overhaul of removing debugging statements, adhering to PEP8, and making most thing lowercase.

--- playback.py
+++ playback.py
@@ -10,7 +10,7 @@ def playnote(noteint, sleeptime):
     # make a list of all files in the directory
     notes = next(os.walk("wav/"))[2]
     notes.sort()
-    subprocess.pOpen(['aplay', '-q', 'wav/' + notes[noteint]])
+    subprocess.Popen(['aplay', '-q', 'wav/' + notes[noteint]])
     sleep(sleeptime)
 
 
@@ -18,20 +18,20 @@ def playnote(noteint, sleeptime):
 def playchordintlist(noteintlist, sleeptime):
     notes = next(os.walk("wav/"))[2]
     for ints in noteintlist:
-        subprocess.pOpen(['aplay', '-q', 'wav/' + notes])
+        subprocess.Popen(['aplay', '-q', 'wav/' + notes])
     sleep(sleeptime)
 
 
 # plays a single note by filename
 def playnotefile(filename, sleeptime):
-    subprocess.pOpen(['aplay', '-q', 'wav/' + filename])
+    subprocess.Popen(['aplay', '-q', 'wav/' + filename])
     sleep(sleeptime)
 
 
 # plays a whole chord by filename
 def playchordfilelist(filenames, sleeptime):
     for notes in filenames:
-        subprocess.pOpen(['aplay', '-q', 'wav/' + notes])
+        subprocess.Popen(['aplay', '-q', 'wav/' + notes])
     sleep(sleeptime)
 
 
